bb696a238926a429df3ccebd3f340dcfe41d0a32
Max1203sr <maksimsr312@gmail.com>
2022-03-21 18:16:31 +0200

    Test Maxim

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,17 +1,18 @@
 package com.company;
 
 public class Client extends Restoran {
+
     private String dish;
 
-    public Client(String name,String dishs)
-    {
+    public Client(String name, String dishs) {
         super(name);
         this.dish = dishs;
     }
 
-    public void Show()
-    {
-        System.out.printf("Client name: %s \t Dish: %s \n " , super.getName(), dish );
-        System.out.printf("Client name: %s \t Dish: %s \n " , super.getName(), dish );
+    public void Show() {
+        System.out.printf("Client name: %s \t Dish: %s \n ", super.getName(), dish);
+        System.out.printf("Client name: %s \t Dish: %s \n ", super.getName(), dish);
+        System.out.printf("Client name: %s \t Dish: %s \n ", super.getName(), dish);
+        System.out.printf("Client name: %s \t Dish: %s \n ", super.getName(), dish);
     }
 }
